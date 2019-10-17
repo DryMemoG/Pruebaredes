87336777bbf57c193967a71157512ccdf119011b
Guillermo Galindo <31331135+DryMemoG@users.noreply.github.com>
2019-10-16 20:48:08 -0600

    Create HolaMundo.cs

---
+++ HolaMundo.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace hlamundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
